5a96346a442af141525549f105e8558c5e9b8ca3
unknown <mycrossstitch@yandex.ru>
2025-09-02 11:53:49 +0300

    Внесены изменения в локаторы, создан pytest.ini

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,11 @@
 from selenium.webdriver.common.by import By
 
-
+class MainPageLocators():
+    pass   # не используем,
 
 class LoginPageLocators():
-    LOGIN_FORM_LINK = (By.CSS_SELECTOR, "#login_form1")
-    REGISTER_FORM_LINK = (By.CSS_SELECTOR, "#register_form1")
+    LOGIN_FORM_LINK = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM_LINK = (By.CSS_SELECTOR, "#register_form")
 
 class ProductPageLocators:
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
--- test_product_page.py
+++ test_product_page.py
@@ -48,3 +48,28 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
     basket_page.should_be_empty_basket_message()
+
+@pytest.mark.xfail(reason="Сообщение об успехе появляется, а его быть не должно")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail(reason="Сообщение об успехе не исчезает само по себе")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_be_disappeared_success_message()
